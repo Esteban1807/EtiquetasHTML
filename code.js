49f249c3476cd3c16c19320eb86bfcd364449fe8
mariaolaya17 <mariadelosangelesolayagar@gmail.com>
2026-03-02 11:08:49 -0500

    Actualizacion de  css y js,Mejorando modo oscuro y botón arriba

--- code.js
+++ code.js
@@ -2,6 +2,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const btnArriba = document.getElementById("btnArriba");
     const botonModo = document.getElementById("modoOscuro");
+    const navbar = document.querySelector("nav");
+
 
     window.addEventListener("scroll", function () {
 
@@ -11,18 +13,52 @@ document.addEventListener("DOMContentLoaded", function () {
             btnArriba.classList.remove("mostrar");
         }
 
+        if (navbar) {
+            if (window.scrollY > 50) {
+                navbar.classList.add("nav-scroll");
+            } else {
+                navbar.classList.remove("nav-scroll");
+            }
+        }
+
     });
 
-    btnArriba.addEventListener("click", function () {
-        window.scrollTo({
-            top: 0,
-            behavior: "smooth"
+    if (btnArriba) {
+        btnArriba.addEventListener("click", function () {
+            window.scrollTo({
+                top: 0,
+                behavior: "smooth"
+            });
         });
-    });
+    }
+
+
+    function actualizarTextoBoton() {
+        if (document.body.classList.contains("dark-mode")) {
+            botonModo.textContent = "Modo Claro ☀️";
+        } else {
+            botonModo.textContent = "Modo Oscuro 🌙";
+        }
+    }
+
+    if (localStorage.getItem("modoOscuro") === "activo") {
+        document.body.classList.add("dark-mode");
+    }
 
     if (botonModo) {
+        actualizarTextoBoton();
+
         botonModo.addEventListener("click", function () {
+
             document.body.classList.toggle("dark-mode");
+
+            if (document.body.classList.contains("dark-mode")) {
+                localStorage.setItem("modoOscuro", "activo");
+            } else {
+                localStorage.removeItem("modoOscuro");
+            }
+
+            actualizarTextoBoton();
         });
     }
 
